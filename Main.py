9fd89c8281bb33ce56048f6da106a6f7cc81b4ed
Stephen N <stephen.nedd@student.hu.nl>
2019-02-09 18:02:17 +0100

    Changed the structure from a given ammount of variables to a text file that the program reads to generate random sentences.

--- Main.py
+++ Main.py
@@ -1,17 +1,17 @@
 import random
 import time
 
-# The list of available variables for sentences, this can be made more random in the future.
-# Maybe add a whole article and create a function that Gets() a random sentence.
-a = "In the morning."
-b = "Encouraging me."
-c = "Brothers and sisters."
-
-# Create's the list of sentences.
-Sentences = [a, b, c]
-# Randomize and print the list.
-random.shuffle(Sentences)
-print(Sentences[0])
+# Open file article.txt and seperate the sentences using ('.') as split.
+# Then write a randomly chosen sentence into sentence.txt.
+for i in range(1000):
+    with open("article.txt", 'r') as f:
+        sentences = f.read().split(".")
+    with open("sentence.txt", 'w') as f:
+        f.write(random.choice(sentences) + ".")
+# Read the sentence that was written into sentences.txt and print it.
+with open("sentence.txt", 'r') as q:
+    sentence = q.read().strip()
+    print(sentence)
 
 # Create a time stamp of the time it printed the sentence.
 start = time.time()
@@ -22,7 +22,7 @@ typedword = input("\033[1;34mPlease retype the above sentence: \033[0m")
 # If user types sentence correctly..
 # Take another timestamp of the time the print happened.
 # Subtract the current time from the start time)
-if typedword == Sentences[0]:
+if typedword == sentence:
     now = time.time()
     end = round((now - start), 2)
     print("\033[1;32m\nThat's correct! \033[0m" + " You took " ,end,  " seconds!")
