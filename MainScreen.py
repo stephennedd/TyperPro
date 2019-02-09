a4c87196a52ac137f594914533e8219a6c19a472
Stephen N <stephen.nedd@student.hu.nl>
2019-02-09 16:35:44 +0100

    Cleaned up code

--- MainScreen.py
+++ MainScreen.py
@@ -1,26 +1,23 @@
 import random
 import time
 
-RED = '\033[91m'
-BLUE = '\033[94m'
-GREEN = '\033[92m'
 
 a = "In the morning."
 b = "Encouraging me."
 c = "Brothers and sisters."
 
-list1 = [a, b, c]
+Sentences = [a, b, c]
 
-random.shuffle(list1)
+random.shuffle(Sentences)
 
-print(list1[0])
+print(Sentences[0])
 
 start = time.time()
 
 
-typedword = input("Please retype the above sentence: ")
+typedword = input("\033[1;34mPlease retype the above sentence: \033[0m")
 
-if typedword == list1[0]:
+if typedword == Sentences[0]:
     now = time.time()
     end = round((now - start), 2)
     print("\033[1;32mThat's correct! \033[0m" + " You took " ,end,  " seconds!")
